acc92c11b50d5f8c1a32c6496b6c03a9b87adde9
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2023-04-12 13:36:50 +0200

    <initializr-end> Codebase generated from Spring Initializr

---
+++ src/main/java/example/billingjob/BillingJobApplication.java
@@ -0,0 +1,13 @@
+package example.billingjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingJobApplication.class, args);
+	}
+
+}
