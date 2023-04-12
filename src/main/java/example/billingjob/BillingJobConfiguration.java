1b92884733e41f2d56bc7a2e582f1949de6744f5
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2023-04-12 13:36:50 +0200

    <initializr-end> Codebase generated from Spring Initializr

---
+++ src/main/java/example/billingjob/BillingJobConfiguration.java
@@ -0,0 +1,10 @@
+package example.billingjob;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BillingJobConfiguration {
+
+  // TODO add job definition here
+
+}
