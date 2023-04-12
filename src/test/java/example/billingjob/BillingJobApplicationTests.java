2d2e3ebdf01c4817ff1f708c96d25f9c582341d1
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2023-04-12 13:36:50 +0200

    <initializr-start> Initial codebase generated from Spring Initializr

---
+++ src/test/java/example/billingjob/BillingJobApplicationTests.java
@@ -0,0 +1,13 @@
+package example.billingjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
