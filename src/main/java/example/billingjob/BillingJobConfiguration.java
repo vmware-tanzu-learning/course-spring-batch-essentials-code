a67081619a98a7684eed3c709882d9cf3efd17a7
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2023-04-28 17:41:53 +0200

    <first-job-end> End point of the first job Lab

--- src/main/java/example/billingjob/BillingJobConfiguration.java
+++ src/main/java/example/billingjob/BillingJobConfiguration.java
@@ -1,10 +1,16 @@
 package example.billingjob;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BillingJobConfiguration {
 
-  // TODO add job definition here
+	@Bean
+	public Job job(JobRepository jobRepository) {
+		return new BillingJob(jobRepository);
+	}
 
 }
--- src/test/java/example/billingjob/BillingJobApplicationTests.java
+++ src/test/java/example/billingjob/BillingJobApplicationTests.java
@@ -1,13 +1,40 @@
 package example.billingjob;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 
 @SpringBootTest
+@ExtendWith(OutputCaptureExtension.class)
 class BillingJobApplicationTests {
 
+	@Autowired
+	private Job job;
+
+	@Autowired
+	private JobLauncher jobLauncher;
+
 	@Test
-	void contextLoads() {
+	void testJobExecution(CapturedOutput output) throws Exception {
+		// given
+		JobParameters jobParameters = new JobParameters();
+
+		// when
+		JobExecution jobExecution = this.jobLauncher.run(this.job, jobParameters);
+
+		// then
+		Assertions.assertTrue(output.getOut().contains("processing billing information"));
+		Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
 	}
 
 }
