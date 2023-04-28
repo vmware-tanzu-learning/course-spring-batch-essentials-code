552ea28bbce628142103ba0b6a42e190ea37af0b
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2023-04-28 17:41:53 +0200

    <first-job-end> End point of the first job Lab

---
+++ src/main/java/example/billingjob/BillingJob.java
@@ -0,0 +1,29 @@
+package example.billingjob;
+
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.repository.JobRepository;
+
+public class BillingJob implements Job {
+
+    private JobRepository jobRepository;
+
+    public BillingJob(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
+    @Override
+    public String getName() {
+        return "BillingJob";
+    }
+
+    @Override
+    public void execute(JobExecution execution) {
+        System.out.println("processing billing information");
+        execution.setStatus(BatchStatus.COMPLETED);
+        execution.setExitStatus(ExitStatus.COMPLETED);
+        this.jobRepository.update(execution);
+    }
+}
