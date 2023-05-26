8cc3522bb4136842fb9b469cc292a48b82c27676
Mahmoud Ben Hassine <mbenhassine@vmware.com>
2023-05-26 11:43:45 +0200

    <job-instances-end> <testing-your-job-start> End point of the job instances Lab

--- src/main/java/example/billingjob/BillingJob.java
+++ src/main/java/example/billingjob/BillingJob.java
@@ -4,6 +4,7 @@ import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.repository.JobRepository;
 
 public class BillingJob implements Job {
@@ -21,7 +22,9 @@ public class BillingJob implements Job {
 
     @Override
     public void execute(JobExecution execution) {
-        System.out.println("processing billing information");
+        JobParameters jobParameters = execution.getJobParameters();
+        String inputFile = jobParameters.getString("input.file");
+        System.out.println("processing billing information from file " + inputFile);
         execution.setStatus(BatchStatus.COMPLETED);
         execution.setExitStatus(ExitStatus.COMPLETED);
         this.jobRepository.update(execution);
--- src/test/java/example/billingjob/BillingJobApplicationTests.java
+++ src/test/java/example/billingjob/BillingJobApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,13 +28,15 @@ class BillingJobApplicationTests {
 	@Test
 	void testJobExecution(CapturedOutput output) throws Exception {
 		// given
-		JobParameters jobParameters = new JobParameters();
+		JobParameters jobParameters = new JobParametersBuilder()
+				.addString("input.file", "/some/input/file")
+				.toJobParameters();
 
 		// when
 		JobExecution jobExecution = this.jobLauncher.run(this.job, jobParameters);
 
 		// then
-		Assertions.assertTrue(output.getOut().contains("processing billing information"));
+		Assertions.assertTrue(output.getOut().contains("processing billing information from file /some/input/file"));
 		Assertions.assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
 	}
 
